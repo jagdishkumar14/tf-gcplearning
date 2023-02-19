172d4f611287902daadf224239c1882511ba492b
jagdish <devops.jagdish.kumar@gmail.com>
2023-02-19 18:11:36 +0530

    allow stopin to true to update instance type

--- vm.tf
+++ vm.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "e2-small"
-
+  allow_stopping_for_update = true
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
