f5aed4ed75f9f179499b4d7fadf54b87376c2846
jagdish <devops.jagdish.kumar@gmail.com>
2023-02-19 18:45:46 +0530

    printing more info in output file

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "instance_type" {
   value = google_compute_instance.vm_instance.machine_type
 }
+output "instance_ipadd" {
+    value = google_compute_instance.vm_instance.network_interface
+  
+}
+output "instance_hostname" {
+    value = google_compute_instance.vm_instance.hostname
+  
+}
