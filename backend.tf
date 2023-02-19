eba7587750ad82bf2994639b68cb525be930111c
jagdish <devops.jagdish.kumar@gmail.com>
2023-02-19 17:51:15 +0530

    Adding gcp credentail variable

---
+++ backend.tf
@@ -0,0 +1,11 @@
+
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io" // for Terraform Cloud, this may be omitted or set to `app.terraform.io`
+    organization = "gcplearning"
+
+    workspaces {
+      name = "tf-gcplearning"
+    }
+  }
+}
